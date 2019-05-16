f97d37cd432f084bf3ef4b790d23b267cee66c4c
Leslie Tang <tangweejieleslie+github@gmail.com>
2019-05-16 15:31:11 +0800

    trying out event listener based on Most's tutorial
    reference: https://www.youtube.com/watch?v=TlB_eWDSMt4

--- node/app.js
+++ node/app.js
@@ -1,20 +1,5 @@
-function hello(name){
-    console.log(`Hello ${name}`);
-}
+const EventEmitter = require('events');
+const Logger = require('./logger')
+const logger = new Logger();
 
-hello('Leslie');
-
-const testMod = require('./testmodule');
-
-console.log(testMod.pvar)
-var privateVariable = "REWRITTEN";
-
-testMod.pvar = privateVariable;
-console.log(testMod.pvar)
-
-testMod.test();
-
-const path = require('path');
-
-// the following illustrates that require will take only exports from the respective module
-console.log(testMod);
+logger.log('THIS IS A TEST MESSAGE');
