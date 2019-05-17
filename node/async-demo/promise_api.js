dfa202650d2fb56046b2bdcfda93067f40cbf57e
Leslie Tang <tangweejieleslie+github@gmail.com>
2019-05-18 06:56:37 +0800

    "Parallel" / Simultaneous Async Promises
    The main thing to note is the usage of Promise.all()
    - useful for aggregating results of multiple promises
    - a single reject will lead to reject of all promises // feels like there's some use case here, e.g. we want a situation where all promises are successful, else send another request to re-run the operation
    
    Reference:
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
    https://hackernoon.com/should-i-use-promises-or-async-await-126ab5c98789
    https://stackoverflow.com/questions/35612428/call-async-await-functions-in-parallel
    
    #async #parallel #concurrent

--- node/async-demo/promise_api.js
+++ node/async-demo/promise_api.js
@@ -10,3 +10,25 @@ p_failure.catch(error=>(console.log(error)));
 
 
 //  Running Promises in Parallel
+const p1 = new Promise((resolve, reject)=>{
+    setTimeout(() =>{
+        console.log('Async Operation 1...');
+        // resolve(1);
+        reject(new Error('Something failed...')); // error will kick in and prevent promise.all from being fulfilled/resolved
+    },2000);
+});
+
+const p2 = new Promise((resolve)=>{
+    setTimeout(() =>{
+        console.log('Async Operation 2...');
+        resolve(2);
+    },2000);
+});
+
+// Return new promise when all promises in the array is resolved
+Promise.all([p1,p2]) // promise.all, promise.race. race will be considered fulfilled if a single parallel async operation is successful
+    .then(result => console.log(result))
+    .catch(error => console.log(error.message));
+
+// Single thread kicks off p1 and p2 almost simultaneously 
+// Result array  
