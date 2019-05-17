a8ac18720a05627a274edae165523b937a6e5ca1
Leslie Tang <tangweejieleslie+github@gmail.com>
2019-05-17 20:14:41 +0800

    one way to approach callback hell
    - by naming all the arrow functions
    - extracting them and supplying them with a reference to another function
    
    #CallbackHell
    #needsRevision
    - I don't really fully grasp this concept yet

--- node/async-demo/index.js
+++ node/async-demo/index.js
@@ -1,28 +1,24 @@
 console.log('Before'); // console will display this first, SYNC
 
-const user = getUser(1, function(user) {
-    console.log('User: ', user);
-
-    getRepo(user.username, (repos)=>{
-        console.log('Repos', repos);
-    });
-}); 
+getUser(1,getRepo);
 
 console.log('After'); // console will display this SECOND, SYNC
 
-
-// Callback = function that will be called when data is ready
-function getUser(id, callback){
-    // Set timeout to simulate long running operation
-    setTimeout(()=>{ 
-        console.log('Reading a user from a database');
-        callback({id: id, username: "twjl"});
-    }, 2000);
+function getRepo(user){
+    getRepo(user.username, getCommits);
 }
 
-function getRepo(username, callback){
-    setTimeout(()=>{
-        console.log('Calling API...');
-        callback(['repo1', 'repo2']);
-    },2000);
+function getCommits(repos){
+    getCommits(repo, displayCommits);
+}
+
+function displayCommits(commits){
+    console.log(commits);
+}
+
+function getUser(id, callback){
+    setTimeout(() =>{
+        console.log('Reading user from db...');
+        // callback({id: id, username: 'ltwj'});
+    },0);
 }
