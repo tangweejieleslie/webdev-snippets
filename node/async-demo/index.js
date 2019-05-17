9dc1c8e5cbb7baec1fa343910f490cb74097d64c
Leslie Tang <tangweejieleslie+github@gmail.com>
2019-05-17 19:40:30 +0800

    the code simulates long wait time
    Reference: https://www.pluralsight.com/guides/introduction-to-asynchronous-javascript

--- node/async-demo/index.js
+++ node/async-demo/index.js
@@ -1,9 +1,21 @@
 console.log('Before'); // console will display this first, SYNC
 
-// Set timeout is Async, non-blocking function
-setTimeout(()=>{
-    console.log('Reading a user from a database')
-}, 2000);
+const user = getUser(1); 
+console.log(user); // will return undefined, because user will be returned 2 seconds later
 
 console.log('After'); // console will display this SECOND, SYNC
 
+
+// Set timeout is Async, non-blocking function, this THIRD, after 2seconds
+// Async != multiple thread 
+function getUser(id){
+    // Set timeout to simulate long running operation
+    setTimeout(()=>{ 
+        console.log('Reading a user from a database');
+        return {id: id, user: "twjl"};
+    }, 2000);
+
+    return 1;
+}
+
+// Call
