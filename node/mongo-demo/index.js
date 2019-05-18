3776070babce76b7133c4d5b43fce4080f49c4f6
Leslie Tang <tangweejieleslie+github@gmail.com>
2019-05-18 09:37:57 +0800

    Setup Connection to MongoDB
    Key notes
    - mongoose.connect() returns a promise
    > implies we need to deal it through then and catch
    > install mongoose compass separately
    > need to figure out configuration to server, etc
    
    Reference:
    https://mongoosejs.com/docs/

---
+++ node/mongo-demo/index.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+
+// Establish connection to Mongoose / MongoDB
+mongoose.connect('mongodb://localhost/sandbox',  {useNewUrlParser: true}) // returns promise
+    .then(()=> console.log('Connected to MongoDB...'))
+    .catch(error => console.error('Could not connect to MongoDB...', error));
