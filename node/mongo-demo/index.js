9ffb990a2df0970684c128e6aae005fa2fd92d70
Leslie Tang <tangweejieleslie+github@gmail.com>
2019-05-18 09:42:20 +0800

    Defined Schema
    basically the template of the data that would be stored in DB

--- node/mongo-demo/index.js
+++ node/mongo-demo/index.js
@@ -4,3 +4,12 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/sandbox',  {useNewUrlParser: true}) // returns promise
     .then(()=> console.log('Connected to MongoDB...'))
     .catch(error => console.error('Could not connect to MongoDB...', error));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now},
+    isPublished: Boolean
+});
+
